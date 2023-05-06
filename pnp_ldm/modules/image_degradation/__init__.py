79a9443298c34ddc04ab629833ef9838b14c10f8
junyi42 <junyizhang@sjtu.edu.cn>
2023-05-06 18:50:10 +0800

    first commit

---
+++ pnp_ldm/modules/image_degradation/__init__.py
@@ -0,0 +1,2 @@
+from pnp_ldm.modules.image_degradation.bsrgan import degradation_bsrgan_variant as degradation_fn_bsr
+from pnp_ldm.modules.image_degradation.bsrgan_light import degradation_bsrgan_variant as degradation_fn_bsr_light
